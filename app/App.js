5b59909becfdb5b2b19ac90c9e1009c72e41b471
Zachary Rose <zachary.f.rose@gmail.com>
2016-02-13 23:48:48 -0700

    Temporarily removed hash history route config for easier local development

--- app/App.js
+++ app/App.js
@@ -2,14 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Router = require('react-router').Router;
 var routes = require('./config/routes');
-//import { browserHistory } from 'react-router';
-import { createHistory } from 'history';
+import { browserHistory } from 'react-router';
+//import { createHistory } from 'history';
 //import createHistory from 'history/lib/createBrowserHistory'
 
-const history = createHistory();
+//const history = createHistory();
 
 ReactDOM.render(
-  //<Router history={browserHistory} routes={routes} />,
-  <Router history={history} routes={routes} />,
+  <Router history={browserHistory} routes={routes} />,
+  //<Router history={history} routes={routes} />,
   document.getElementById('app')
 )
