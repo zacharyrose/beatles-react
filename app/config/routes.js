b2aa483167257aa76a2803cbf4544332ff2a358e
Zachary Rose <zachary.f.rose@gmail.com>
2016-03-01 11:56:28 -0700

    refactored App and routes to ES6 classes

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,10 @@
-var React = require('react');
-var Main = require('../components/Main');
-var Home = require('../components/Home');
-var Cart = require('../components/Cart');
-var Shop = require('../components/Shop');
-var Router = require('react-router');
-var Route = Router.Route;
-var IndexRoute = Router.IndexRoute;
+import React from 'react';
+import { Router, Route, IndexRoute } from 'react-router'
+
+import Main from '../components/Main';
+import Home from '../components/Home';
+import Cart from '../components/Cart';
+import Shop from '../components/Shop';
 
 module.exports = (
   <Route path="/" component={Main}>
